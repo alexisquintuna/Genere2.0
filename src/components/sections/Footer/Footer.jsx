2dc4b855af5d52e24e045152bca345dc7f1062e7
Alexis Quintuna <alexisiquintuna@gmail.com>
2022-07-06 19:41:42 -0400

    updated the footer section layout

--- src/components/sections/Footer/Footer.jsx
+++ src/components/sections/Footer/Footer.jsx
@@ -1,33 +1,75 @@
-import "./footer.css"
+import "./footer.css";
 import logo from "../../images/logo.jpeg";
-
+import {AiOutlineArrowRight} from "react-icons/ai"
 
 const Footer = () => {
-    return (
-        <footer>
-            <div>
-                <div>
-                <img src={logo} alt="Logo" />
-                </div>
-                <a href="#"><strong>Genere natuS</strong></a>
-            </div>
+  return (
+    <footer>
 
-            <ul>
-                <li>
-                    <a href="#Welcome">Welcome</a>
-                </li>
-                <li>
-                    <a href="#About">About</a>
-                </li>
-                <li>
-                    <a href="#Pricing">Pricing</a>
-                </li>
-                <li>
-                    <a href="#Contact">Contact</a>
-                </li>
-            </ul>
-        </footer>
-    )
-}
+      <div className="footer__border">
+
+        <div className="footer__logo">
+
+          <div className="footer__logo-pic">
+            <img
+              className="footer__pic"
+              src={logo}
+              alt="Logo"
+            />
+          </div>
+
+          <a className="footer__logo-name" href="#home">
+            Genere natuS
+          </a>
+
+          <p className="copy">© 2022 </p>
+          
+        </div>
+
+        <ul className="socialMenu">
+          <li>
+            <a href="https://www.instagram.com/" className="footer__instagram">INSTAGRAM</a>
+          </li>
+          <li>
+            <a href="http://youtube.com/" className="footer__youtube">YOUTUBE</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/" className="footer__twitter">TWITTER</a>
+          </li>
+        </ul>
+
+        <ul className="menu">
+          <li>
+            <a href="#Welcome">W</a>
+          </li>
+          <li>
+            <a href="#About">A</a>
+          </li>
+          <li>
+            <a href="#Pricing">P</a>
+          </li>
+          <li>
+            <a href="#Contact">C</a>
+          </li>
+        </ul>
+
+        <div className="subscription">
+          <p class="subscribe">Subscribe to our newsletter</p>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="footer__input"
+            placeholder="your@email.com"
+            autocomplete="off"
+          />
+          <AiOutlineArrowRight className="footer__arrow" />
+        </div>
+
+      </div>
+
+    </footer>
+  );
+};
 
 export default Footer;
